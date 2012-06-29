7c7dd88a9e21b7a4767aca4c98535639905b36a4
Andrey Raygorodskiy <andrey.raygorodskiy@gmail.com>
2012-06-29 19:12:20 +0400

    small changes

--- android-client/src/com/artezio/MainActivity.java
+++ android-client/src/com/artezio/MainActivity.java
@@ -1,7 +1,10 @@
 package com.artezio;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +14,27 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        /*final Button button = (Button) findViewById(R.id.buttonscan);
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(intent, 0);
+            }
+        });*/
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                System.out.println(contents);
+                System.out.println(format);
+                // Handle successful scan
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
     }
 }
