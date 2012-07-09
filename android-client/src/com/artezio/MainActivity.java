b0a87918637eff58345891c680c19ae231c6b61a
Andrey Raygorodskiy <andrey.raygorodskiy@gmail.com>
2012-07-09 16:44:58 +0400

    add activity for prices (mock)

--- android-client/src/com/artezio/MainActivity.java
+++ android-client/src/com/artezio/MainActivity.java
@@ -3,10 +3,17 @@ package com.artezio;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+
+    private EditText text;
+    private ImageButton buttonSearch;
+
     /**
      * Called when the activity is first created.
      */
@@ -14,14 +21,32 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        /*final Button button = (Button) findViewById(R.id.buttonscan);
+        text = ((EditText) findViewById(R.id.barcodeText));
+        text.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                buttonSearch.setEnabled(text.getText().length() > 0);
+                return false;
+            }
+        });
+        final ImageButton button = (ImageButton) findViewById(R.id.buttonscan);
         button.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+//                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
                 startActivityForResult(intent, 0);
             }
-        });*/
+        });
+        buttonSearch = (ImageButton) findViewById(R.id.buttonSearch);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, PricesListActivity.class);
+                myIntent.putExtra("SCAN_RESULT", text.getText());
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+        buttonSearch.setEnabled(text.getText().length() > 0);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -29,12 +54,17 @@ public class MainActivity extends Activity {
             if (resultCode == RESULT_OK) {
                 String contents = intent.getStringExtra("SCAN_RESULT");
                 String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                System.out.println(contents);
-                System.out.println(format);
-                // Handle successful scan
-            } else if (resultCode == RESULT_CANCELED) {
-                // Handle cancel
+                text.setText(contents);
             }
+//            else if (resultCode == RESULT_CANCELED) {
+//            }
         }
+        buttonSearch.setEnabled(text.getText().length() > 0);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        buttonSearch.setEnabled(text.getText().length() > 0);
     }
 }
---
+++ android-client/src/com/artezio/PricesListActivity.java
@@ -0,0 +1,52 @@
+package com.artezio;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: araigorodskiy
+ * Date: 09.07.12
+ * Time: 15:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class PricesListActivity extends Activity{
+    private ArrayAdapter<BasicNameValuePair> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prices);
+        final List<BasicNameValuePair> list = new ArrayList<BasicNameValuePair>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new BasicNameValuePair("label "+i, "text "+i));
+        }
+        adapter = new ArrayAdapter<BasicNameValuePair>(this,android.R.layout.simple_list_item_2,list){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                TwoLineListItem row;
+                if(convertView == null){
+                    LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    row = (TwoLineListItem)inflater.inflate(android.R.layout.simple_list_item_2, null);
+                }else{
+                    row = (TwoLineListItem)convertView;
+                }
+                BasicNameValuePair data = list.get(position);
+                row.getText1().setText(data.getName());
+                row.getText2().setText(data.getValue());
+
+                return row;
+            }
+        };
+        ((ListView)findViewById(R.id.listView)).setAdapter(adapter);
+    }
+}
